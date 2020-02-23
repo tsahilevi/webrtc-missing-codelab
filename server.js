c8ebeb1ee1bc132bb48082c36dadd9e29ad9cc98
Philipp Hancke <philipp.hancke@googlemail.com>
2020-02-23 16:50:59 +0100

    server: document some decisions

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const uuid = require('uuid');
 
 const port = 8080;
  
+// We use a HTTP server for serving static pages. In the real world you'll
+// want to separate the signaling server and how you serve the HTML/JS, the
+// latter typically through a CDN.
 const server = http.Server({})
     .listen(port);
 server.on('listening', () => {
@@ -23,17 +26,24 @@ server.on('request', (request, response) => {
         response.end(data);
     });
 });
-const wss = new WebSocket.Server({server});
 
 // A map of websocket connections.
 const connections = new Map();
+// WebSocket server, running alongside the http server.
+const wss = new WebSocket.Server({server});
+
+// Generate a (unique) client id.
+// Exercise: extend this to generate a human-readable id.
+function generateClientId() {
+    // TODO: enforce uniqueness here instead of below.
+    return uuid.v4();
+}
  
 wss.on('connection', (ws) => {
-    const id = uuid.v4();
     // Assign an id to the client. The other alternative is to have the client
     // pick its id and tell us. But that needs handle duplicates. It is preferable
     // if you have ids from another source but requires some kind of authentication.
-    ws.id = id;
+    const id = generateClientId();
     console.log(id, 'Received new connection');
 
     if (connections.has(id)) {
@@ -50,9 +60,9 @@ wss.on('connection', (ws) => {
         id,
     }));
 
+    // Remove the connection. Note that this does not tell anyone you are currently in a call with
+    // that this happened. This would require additional statekeeping that is not done here.
     ws.on('close', () => {
-        // Remove the connection. Note that this does not tell anyone you are currently in a call with
-        // that this happened. This would require additional statekeeping that is not done here.
         console.log(id, 'Connection closed');
         connections.delete(id); 
     });
@@ -60,6 +70,8 @@ wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         console.log(id, 'received', message);
         let data;
+        // TODO: your protocol should send some kind of error back to the caller instead of
+        // returning silently below.
         try  {
             data = JSON.parse(message);
         } catch (err) {
@@ -68,16 +80,24 @@ wss.on('connection', (ws) => {
         }
         if (!data.id) {
             console.log(id, 'missing id', data);
+            return;
         }
+
+        // The direct lookup of the other clients websocket is overly simplified.
+        // In the real world you might be running in a cluster and would need to send
+        // messages between different servers in the cluster to reach the other side.
         if (!connections.has(data.id)) {
             console.log(id, 'peer not found', data.id);
+            // TODO: the protocol needs some error handling here. This can be as
+            // simple as sending a 'bye' with an extra error element saying 'not-found'.
             return;
         }
         const peer = connections.get(data.id);
 
-        // stamp messages with our id.
+        // Stamp messages with our id. In the client-to-server direction, 'id' is the
+        // client that the message is sent to. In the server-to-client direction, it is
+        // the client that the message originates from.
         data.id = id;
-        // This is overly simplified.
         peer.send(JSON.stringify(data), (err) => {
             if (err) {
                 console.log(id, 'failed to send to peer', err);
