e7ab1cb9b931b10b846a821ccfbc415fd155442c
Philipp Hancke <philipp.hancke@googlemail.com>
2020-06-09 12:26:21 +0200

    no-autodial: add support for codec selection
    adds support for selecting the video codec to the no-autodial page.
    This demonstrates using the RTCRtpTransceiver.setCodecPreferences API
    to select a codec and RTCRtpSender.getCapabilities("video") to enumerate
    available codecs.
    
    SDP munging (on the remote description) will be needed if that API is
    not available.

--- static/main.js
+++ static/main.js
@@ -101,6 +101,24 @@ bandwidthSelector.onchange = () => {
     });
 }
 
+// Change the video codec.
+const codecPreferences = document.querySelector('#codecPreferences');
+const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
+  'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
+if (supportsSetCodecPreferences && codecPreferences) {
+    const {codecs} = RTCRtpSender.getCapabilities('video');
+    codecs.forEach(codec => {
+        if (['video/red', 'video/ulpfec', 'video/rtx'].includes(codec.mimeType)) {
+            return;
+        }
+        const option = document.createElement('option');
+        option.value = (codec.mimeType + ' ' + (codec.sdpFmtpLine || '')).trim();
+        option.innerText = option.value;
+        codecPreferences.appendChild(option);
+    });
+    codecPreferences.disabled = false;
+}
+
 const connectionState = document.getElementById('connectionState');
 
 // We connect to the same server and same protocol. Note that in production
@@ -371,6 +389,22 @@ async function call(id) {
     if (localStream) {
         localStream.getTracks().forEach(t => pc.addTrack(t, localStream));
     }
+
+    // Change the codec. Only on the no-autodial page.
+    if (supportsSetCodecPreferences && codecPreferences) {
+        const preferredCodec = codecPreferences.options[codecPreferences.selectedIndex];
+        if (preferredCodec.value !== '') {
+            const [mimeType, sdpFmtpLine] = preferredCodec.value.split(' ');
+            const {codecs} = RTCRtpSender.getCapabilities('video');
+            const selectedCodecIndex = codecs.findIndex(c => c.mimeType === mimeType && c.sdpFmtpLine === sdpFmtpLine);
+            const selectedCodec = codecs[selectedCodecIndex];
+            codecs.slice(selectedCodecIndex, 1);
+            codecs.unshift(selectedCodec);
+            const transceiver = pc.getTransceivers().find(t => t.sender && t.sender.track === localStream.getVideoTracks()[0]);
+            transceiver.setCodecPreferences(codecs);
+            console.log('Preferred video codec', selectedCodec);
+        }
+    }
     const offer = await pc.createOffer();
     await pc.setLocalDescription(offer);
     ws.send(JSON.stringify({
@@ -412,6 +446,8 @@ function hangup(id) {
         type: 'bye',
         id,
     }));
+    if (codecPreferences)
+        codecPreferences.disabled = !supportSetCodecPreferences;
 }
 
 window.addEventListener('beforeunload', () => {
